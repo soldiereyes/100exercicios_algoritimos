5f77ccc747add428e3d5275e1333e7cbda5386fe
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2022-02-28 21:18:56 -0300

    Iniciando lista de exercicios do IFG BA

---
+++ src/com/br/listaExercicios/Lista03/AreaDoRetangulo.java
@@ -0,0 +1,23 @@
+package com.br.listaExercicios.Lista03;
+
+/**
+ * Dado o tamanho da base e da altura do retangulo, calcular a sua area e o seu perimetro
+ */
+public class AreaDoRetangulo {
+    int lado1DoRetangulo;
+    int lado2DoRetangulo;
+
+    static int calculaArea(int lado1DoRetangulo, int lado2DoRetangulo){
+       int  areaDoRetangulo = lado1DoRetangulo * lado2DoRetangulo;
+       return areaDoRetangulo;
+    }
+    static int calculaPerimetro(int lado1DoRetangulo,int lado2DoRetangulo){
+       int perimetroDoRetangulo =2*( lado1DoRetangulo + lado2DoRetangulo);
+        return perimetroDoRetangulo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculaArea(5, 10 ));
+        System.out.println(calculaPerimetro(5, 10));
+    }
+}
---
+++ src/com/br/listaExercicios/Lista03/ConversaoDeNumeros.java
@@ -0,0 +1,44 @@
+package com.br.listaExercicios.Lista03;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Com base em uma frase digitada pelo usuário
+ * 1- Converter a letra de cada palavra para maiúscula e exibir o resultado
+ * 2- Exibir a quantidade de vogais, consoantes e numeros presentes na frase
+ * 3- Exibir a maior palavra da frase
+ * 4- Exibir as palavras de tamanho par
+ * 5- Identificar se a frase é um polindromo
+ */
+
+//public class ConversaoDeNumeros {
+//
+//    public String frase;
+//    public int numeroDeCaracteres = 0;
+//    String [] palavras;
+//
+//
+//    public void main(String[] args) {
+//        Scanner recebeFrase = new Scanner(System.in);
+//        System.out.println("Digite uma frase: ");
+//        String armazenaFrase = recebeFrase.next();
+//
+//
+//        System.out.println(armazenaFrase);
+//
+//         converterFraseEmPalavras(armazenaFrase);
+//
+//    }
+//
+//    public int converterFraseEmPalavras(String fraseArmazenada) {
+//        int posicaoinicialDaPalavra = 0;
+//        int indiceDaPalavra = 0;
+//
+//        for (int i = 0; i < numeroDeCaracteres; i++) {
+//            if (fraseArmazenada == " "){
+////                palavras[indiceDaPalavra] =
+//
+//            }
+//        }
+//    }
+//}
